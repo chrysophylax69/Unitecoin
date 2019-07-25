b8398a13d4d14d37595f0147e1261cdd307de035
chrysophylax chrysxanthes chrysxanthos <chrysophylax69@users.noreply.github.com>
2019-07-25 22:34:11 +0930

    UpDate SeedNodes ... #crysx
    UpDate SeedNodes to CWI Seeds ... #crysx

--- src/net.cpp
+++ src/net.cpp
@@ -1197,9 +1197,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"unitecoin.com", "dnsseed.unitecoin.com"},
-    {"main.unitecoin.info", "dnsseed.unitecoin.info"},
-    {"backup.unitecoin.info", "autodiscover.unitecoin.info"},
+    {"uni-seed01.chainworksindustries.com", "uni-seed01.chainworksindustries.com"},
     {NULL, NULL}
 };
 
